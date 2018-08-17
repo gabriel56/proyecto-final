98e8151e4bd7d049682656d8fb049e46b57648b1
Gabriel <gabrielfrancolino@gmail.com>
2018-08-17 12:07:43 -0300

    Revert "Commit - configuraciónes iniciales"
    This reverts commit efe84157063e7d9534578b14b0d6990907ea1835.

--- index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/tweet');
-
-mongoose.connection
-    .once('open', () => console.log('Conexión con la base de datos establecida!'))
-    .on('error', error => console.log(error));
-
-
--- src/models/models.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-
-mongoose.connection
-    .once('open', () => console.log('Conexión con la base de datos establecida!'))
-    .on('error', error => console.log(error));
-
-
